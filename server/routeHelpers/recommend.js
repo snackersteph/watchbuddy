68dc49259d06a42ff61ca9a5c776fc4e6a18d452
Benjamin Stephens <17017460+speckospock@users.noreply.github.com>
2017-10-10 16:57:15 -0700

    Refactors recommend

--- server/routeHelpers/recommend.js
+++ server/routeHelpers/recommend.js
@@ -2,31 +2,29 @@ const moviedb = require('../../helper/moviedb.js');
 
 //This function is to retrieve the top 5 recommended shows from the MovieDB api for the front page of WatchBuddy.
 module.exports = (req, res) => {
-	var genres;
 	moviedb.genre((data) => {
-		genres = JSON.parse(data).genres
+		const { genres } = JSON.parse(data);
 		moviedb.getPopularShows((data) => {
-			var top = JSON.parse(data).results.splice(0, 5)
-			var array = [];
-			top.forEach((el) => {
-				var obj = {};
-				obj.name = el.name;
-				var arr = [];
-				el.genre_ids.forEach((int) => {
+			const top = JSON.parse(data).results.splice(0, 5);
+			const array = top.map(show => {
+				const { name, id } = show;
+				const arr = show.genre_ids.map(int => {
 					for (var i = 0; i < genres.length; i++) {
 						if (genres[i].id === int){
-							arr.push(genres[i].name)
+							return genres[i].name;
 						}
 					}
-				})
-				obj.genres = arr
-				obj.summary = el.overview;
-				obj.firstAirDate = el.first_air_date;
-				obj.image = "https://image.tmdb.org/t/p/w500/" + el.backdrop_path;
-				obj.id = el.id
-				array.push(obj)
-			})
+				});
+				return ({
+					name,
+					id,
+					genres: arr,
+					summary: show.overview,
+					firstAirDate: show.first_air_date,
+					image: `https://image.tmdb.org/t/p/w500/${show.backdrop_path}`,
+				});
+			});
 			res.send(array)
-		})
-	})
+		});
+	});
 }
