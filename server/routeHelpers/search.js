2d95576ddde0b461913df9d13b4705beb3aa4a1e
Benjamin Stephens <17017460+speckospock@users.noreply.github.com>
2017-10-10 16:10:56 -0700

    Refactors search.js

--- server/routeHelpers/search.js
+++ server/routeHelpers/search.js
@@ -1,33 +1,31 @@
 const moviedb = require('../../helper/moviedb.js');
+const { extend } = require('lodash');
 
 //This function will search for the top 10 results when inputting a term into the MovieDB api.
 module.exports = (req, res) => {
-	console.log(req.body.term)
-	var title = req.body.term
+	console.log(req.body.term);
+	var title = req.body.term;
 	var genres;
-	moviedb.genre((data) => {
-		genres = JSON.parse(data).genres
-		moviedb.search(title, (data) => {
+	moviedb.genre(data => {
+		genres = JSON.parse(data).genres;
+		moviedb.search(title, data => {
 			var searched = JSON.parse(data).results.splice(0, 10);
-			var array = [];
-			searched.forEach((el) => {
-				var obj = {};
-				obj.name = el.name;
-				obj.id = el.id;
-				var arr = [];
-				el.genre_ids.forEach((int) => {
-					for (var i = 0; i < genres.length; i++) {
-						if (genres[i].id === int) {
-							arr.push(genres[i].name)
-						}
-					}
-				})
-				obj.genres = arr
-				obj.summary = el.overview;
-				obj.firstAirDate = el.first_air_date;
-				obj.image = "https://image.tmdb.org/t/p/w500/" + el.backdrop_path;
-				array.push(obj)
-			})
+			var array = searched.map(el => {
+				const { name, id } = el;
+				const arr = el.genre_ids.reduce((memo, int) => {
+					genres.filter(genre => genre.id === int).map(genre => memo.push(genre.name));
+					return memo;
+				}, []);
+
+				return ({
+					name,
+					id,
+					genres: arr,
+					summary: el.overview,
+					firstAirDate: el.first_air_date,
+					image: `https://image.tmdb.org/t/p/w500/${el.backdrop_path}`,
+				});
+			});
 			res.send(array);
 		})
 	})
--- server/routeHelpers/signUp.js
+++ server/routeHelpers/signUp.js
@@ -3,14 +3,12 @@ const utils = require('../hashUtils.js');
 
 //This function will create a new row for the users table for new users.
 module.exports = (req, res) => {
-  var salt = utils.createRandom32String(); // create salt
-  var user = req.body.email;
-  var pw = utils.createHash(req.body.password, salt);
-  var array = [user, pw, salt];
-  // array.push(user);
-  // array.push(pw);
-  // array.push(salt);
+  const { email, password } = req.body;
+  const salt = utils.createRandom32String(); // create salt
+  const pw = utils.createHash(password, salt);
+  const array = [email, pw, salt];
+
   db.createUser(array, (data) => {
-     res.send(user);
-  })
+     res.send(email);
+  });
 }
