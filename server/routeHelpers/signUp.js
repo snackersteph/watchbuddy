fb9dc5a3fdce1d55745a8132298fa75ec95506a3
Benjamin Stephens <17017460+speckospock@users.noreply.github.com>
2017-10-11 16:44:09 -0700

    Refactors Signup component to use same variable names as server

--- server/routeHelpers/signUp.js
+++ server/routeHelpers/signUp.js
@@ -3,6 +3,7 @@ const utils = require('../hashUtils.js');
 const { User } = require('../../database-postgres');
 
 //This function will create a new row for the users table for new users.
+<<<<<<< HEAD
 module.exports = ({ body: { 
   username, 
   password,
@@ -17,10 +18,15 @@ module.exports = ({ body: {
   //   avatarUrl,
   // } = req.body;
   console.log('RECEIVED', username, password, phone, avatarUrl);
+=======
+module.exports = (req, res) => {
+  const { username, password } = req.body;
+>>>>>>> Refactors Signup component to use same variable names as server
   const salt = utils.createRandom32String(); // create salt
   const pw = utils.createHash(password, salt);
   const array = [username, pw, salt];
 
+<<<<<<< HEAD
   User.create(({
     username,
     phone,
@@ -34,4 +40,9 @@ module.exports = ({ body: {
   // db.createUser(array, (data) => {
   //    res.send(username);
   // });
+=======
+  db.createUser(array, (data) => {
+     res.send(username);
+  });
+>>>>>>> Refactors Signup component to use same variable names as server
 }
