aeb9fbd550f177b8dcb879fb55ab8ecfeeb7ae5d
speckospock <17017460+speckospock@users.noreply.github.com>
2017-10-12 16:52:10 -0700

    Updates movie schema

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -16,38 +16,3 @@ module.exports = {
   Show,
   Movie,
 };
-
-// const testShow = new Show({
-//   title: 'Breaking Bad',
-//   rating: 5,
-//   episodes: [{
-//     title: 'Pilot',
-//     rating: 4,
-//     review: 'Good, but not as good as the show',
-//     season: 1,
-//     episode: 1,
-//     runtime: 44,
-//   }]
-// });
-
-// testShow.save(err => console.log(err));
-
-// Show.find(err => console.log(err))
-//   .then(shows => {
-//     console.log('found: ', shows);
-//     shows.map(show => {
-//       console.log(show.episodes);
-//       show.remove()
-//     });
-//   });
-
-// const testEvent = new Event({title: 'A TEST'});
-
-// testEvent.save(err => console.log(err));
-
-// Event.find((err, events) => {
-//   if (err) return console.log(err);
-//   console.log('found events: ', events);
-//   events.map(event => event.remove());
-//   console.log('updated events: ', events);
-// });
