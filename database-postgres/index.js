b7cb9015392224c1d55b2996259baf1ec834194e
Benjamin Stephens <17017460+speckospock@users.noreply.github.com>
2017-10-12 10:50:42 -0700

    Updates posgres index, adds instructions to install

--- database-postgres/index.js
+++ database-postgres/index.js
@@ -1,21 +1,45 @@
 const Sequelize = require('sequelize');
 const { userSchema } = require('./schema');
+const { POSTGRES: { USER, PASSWORD, HOST }} = require('../config');
 
-const sequelize = new Sequelize('postgres://bs@localhost:5432/watchbuddy');
+const sequelize = new Sequelize('watchpotato', USER, PASSWORD, {
+  host: HOST,
+  dialect: 'postgres',
+});
 
 const User = sequelize.define('user', userSchema);
 
 User.sync();
 
-// User
-//   .sync({force:true})
-//   .then(() => User.create({
-//     username: '3',
-//     phone: '8675309',
-//   }))
-//   .then(() => User.findAll())
-//   .then(users => console.log(users));
-
 module.exports = {
   User
 };
+
+/**
+ * /////////////////////
+ * // POSTGRES SETUP: //
+ * /////////////////////
+ * 
+ * (NB: Bash commands preceded by $, ex: $ echo 'hello world')
+ * 
+ * 1. Install Homebrew
+ * 2. Install Postgres with Homebrew: 
+ *      $ brew install postgres
+ * 3. Create a new Postgres DB cluster: 
+ *      $ initdb /usr/local/var/postgres
+ * 4. Create a 'postgres' user (fill in version w/ your postgres version): 
+ *      $ /usr/local/Cellar/postgresql/<version>/bin/createuser -s postgres
+ * 5. Start the server: 
+ *      $ brew services start postgresql
+ * 6. Create the 'watchpotato' database using 'postgres' user: 
+ *      $ createdb -O postgres watchpotato
+ * 
+ * --To view the tables (schema),
+ *   - type: 'psql -U postgres'
+ *   - followed by '\c watchpotato'
+ *   - followed by '\dt'
+ *   - to see all rows in a table: 'SELECT * FROM <tablename>;'
+ * --To drop the database,
+ *   - (may need to run: $ brew services stop postgresql followed by $ brew services start postgresql to log all users off 'watchpotato')
+ *   - dropdb cheapeats
+ */
