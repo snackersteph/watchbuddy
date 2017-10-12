a7a392039b691935cc5ea6587763a4d005bebc0b
TiffyB <tiffanyabarth@gmail.com>
2017-10-12 12:10:29 -0700

    fixed styling

--- react-client/src/components/MovieTvTab.jsx
+++ react-client/src/components/MovieTvTab.jsx
@@ -25,16 +25,16 @@ class MovieTvTab extends Component {
           <Menu.Item name='movies' active={activeItem === 'movies'} onClick={this.handleItemClick} />
         </Menu>
         <div> {activeItem === 'TV'
-          ? <Segment attached='bottom' inverted>
+          ? 
               <Grid celled>
                 { this.props.showList.map((show, i) => <ShowEntry show={show} key={i} getShow={this.props.getShow} loggedIn={this.props.loggedIn} addShow={this.props.addShow} isTVShow={true}/>) }
               </Grid>
-            </Segment>
-          : <Segment attached='bottom' inverted>
+            
+          : 
               <Grid celled>
                 { this.props.movieList.map((show, i) => <ShowEntry show={show} key={i} getShow={this.props.getShow} loggedIn={this.props.loggedIn} addShow={this.props.addShow} isTVShow={false}/>) }
               </Grid>
-            </Segment>}
+            }
         </div>
       </div>
     )
--- react-client/src/components/ShowList.jsx
+++ react-client/src/components/ShowList.jsx
@@ -52,9 +52,7 @@ class ShowList extends Component {
       </Segment> 
     : <div> { this.state.loaded === 'true' 
         ? <Segment inverted>
-            <MovieTvTab showList={this.state.showList} movieList={this.state.movieList} getShow={this.props.getShow} loggedIn={this.props.loggedIn} addShow={this.props.addShow}>
-              
-            </MovieTvTab>
+            <MovieTvTab showList={this.state.showList} movieList={this.state.movieList} getShow={this.props.getShow} loggedIn={this.props.loggedIn} addShow={this.props.addShow} />
           </Segment> 
       : <Segment>
           <Dimmer active>
