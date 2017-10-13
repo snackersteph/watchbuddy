501ccf2199c67bf5e5ca785771d9e0a5b5898c43
speckospock <17017460+speckospock@users.noreply.github.com>
2017-10-12 18:36:51 -0700

    Implements instructions for mongo server

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -16,3 +16,38 @@ module.exports = {
   Show,
   Movie,
 };
+
+const simpsons = new Show({
+  title: 'The Simpsons',
+  userId: 3,
+  rating: 5,
+  episodes: [
+    {
+    title: 'Pilot',
+    season: 1,
+    episode: 1,
+    },
+    {
+      title: 'Homer vs. New York',
+      season: 9,
+      episode: 6,
+    },
+    {
+      title: 'Pilot',
+      season: 1,
+      episode: 1,
+    },
+  ]
+});
+
+/**
+ * ////////////////////////
+ * // MONGO INSTRUCTIONS //
+ * ////////////////////////
+ * 
+ * You must have MongoDB installed and running on your local machine for this to work.
+ * For installation instructions @see https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x/
+ * For cloud-based DB implementation @see https://www.mongodb.com/cloud/atlas
+ * 
+ * Otherwise, start a local server with $ mongod or $ sudo mongod
+ */
