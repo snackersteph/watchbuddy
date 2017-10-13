76b48bc54ddf4e661de6ea6814eaa948133b90b8
Devon Yu <devonyu415@gmail.com>
2017-10-13 11:52:01 -0700

    implemented skeleton profile to render hard coded movies that they have already reviewed

--- react-client/src/Profile.jsx
+++ react-client/src/Profile.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
-import { Container, Button, Form, Grid, Header, Image, Message, Segment, Icon, Transition } from 'semantic-ui-react';
+import { Table, Rating, Container, Button, Form, Grid, Header, Image, Message, Segment, Icon, Transition } from 'semantic-ui-react';
 import NavBar from './components/Navbar.jsx';
+import data from './sampleData.js';
 
 class Profile extends Component {
 	constructor(props) {
@@ -10,9 +11,21 @@ class Profile extends Component {
 			name: 'Devon',
 			avatar: 'https://i.imgur.com/bVhY86x.jpg',
 			bio: 'I created an account on Watch Potato so that I can watch my shows at specific times, have someone remind me to watch and even rate them!',
-			ratedShowsMovies: ['the matrix', 'lion king', 'gladiator']
-		}
-	}
+			ratedShowsMovies: data
+    }
+    this.updateRating = this.updateRating.bind(this);
+    this.updateReview = this.updateReview.bind(this);
+  }
+  
+  updateRating() {
+    //this will route to server to update rating for user-movie/show
+  }
+
+  updateReview() {
+    //this will route to server to update review for user-move/show
+  }
+
+
 	render () {
 		return (
 			<div>
@@ -27,23 +40,65 @@ class Profile extends Component {
 			       </Grid.Column>
 			       <Grid.Column>
 				   	  <Container>
-			          <Message>
+			          <Message> 
                   <Message.Header>
                   Welcome back, {this.state.name}
                   </Message.Header>
+                  <p>{this.state.name}'s Bio</p>
                   <p>{this.state.bio}</p>
                 </Message>
 			        </Container>
+
 			       </Grid.Column>
 			      </Grid.Row>
 		  		</Grid>
 
-
-
-			
-
-
-
+          <Table celled padded>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Movie Poster</Table.HeaderCell>
+              <Table.HeaderCell>Title</Table.HeaderCell>
+              <Table.HeaderCell>Your Rating</Table.HeaderCell>
+              <Table.HeaderCell>MovieDB Ratings</Table.HeaderCell>
+              <Table.HeaderCell>Your Review</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+      
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>
+              <Image src={this.state.ratedShowsMovies[0].poster}/>
+              </Table.Cell>
+              <Table.Cell singleLine>{this.state.ratedShowsMovies[0].title}</Table.Cell>
+              <Table.Cell>
+                <Rating icon='star' defaultRating={this.state.ratedShowsMovies[0].rating} maxRating={5} />
+              </Table.Cell>
+              <Table.Cell textAlign='right'>
+                  80% <br />
+                <a href='#'>18 studies</a>
+              </Table.Cell>
+              <Table.Cell>
+                {this.state.ratedShowsMovies[0].review}
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>
+              <Image src={this.state.ratedShowsMovies[1].poster}/>
+              </Table.Cell>
+              <Table.Cell singleLine>{this.state.ratedShowsMovies[1].title}</Table.Cell>
+              <Table.Cell>
+                <Rating icon='star' defaultRating={this.state.ratedShowsMovies[1].rating} maxRating={5} />
+              </Table.Cell>
+              <Table.Cell textAlign='right'>
+                  100% <br />
+                <a href='#'>65 studies</a>
+              </Table.Cell>
+              <Table.Cell>
+                {this.state.ratedShowsMovies[1].review}
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
 
 			</div>
 		)
---
+++ react-client/src/sampleData.js
@@ -0,0 +1,17 @@
+const data = [
+  {
+    id: 0,
+    title: 'Lion King',
+    rating: 4,
+    review: 'The music score for this magnificant movie was outstanding.  The animation is state of the art (at least when this movie came out!',
+    poster: 'https://image.tmdb.org/t/p/w500//mVr0UiqyltcfqxbAUcLl9zWL8ah.jpg'
+  },
+  {
+    title: 'IT',
+    rating: 5,
+    review: 'scary movie here!',
+    poster: 'https://image.tmdb.org/t/p/w500//tcheoA2nPATCm2vvXw2hVQoaEFD.jpg'
+  }
+];
+
+export default data;
