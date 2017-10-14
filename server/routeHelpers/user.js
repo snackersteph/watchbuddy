23eed89009775ed6b152c2f9dcabdadcbff549d3
speckospock <17017460+speckospock@users.noreply.github.com>
2017-10-13 19:54:34 -0700

    /user endpoint successfully adds/updates data from the DB

--- server/routeHelpers/user.js
+++ server/routeHelpers/user.js
@@ -11,13 +11,23 @@ module.exports = {
           events: Event.find({ userId: id }).exec(),
           shows: Show.find({ userId: id }).exec(),
           movies: Movie.find({ userId: id }).exec(),
-        }).then(result => console.log(result));
-        res.send(JSON.stringify(id));
+        }).then(result => res.send(200, result))
+        .catch(err => res.send(err));
+        // res.send(JSON.stringify(id));
       });
   },
   POST: ({ body: { events = [], shows = [], movies = [] }, params: { username }}, res) => {
+    if (!(events.length || shows.length || movies.length)) {
+      res.send('Nothing to change');
+    }
     User.findOne({ where: { username }})
       .then(({ dataValues: { id }}) => {
+        events.map(event => Event.update({ userId: id, title: event.title }, event, { upsert: true })
+          .then(result => console.log(result)));
+        shows.map(show => Show.update({ userId: id, title: show.title }, show, { upsert: true })
+          .then(result => console.log(result)));
+        movies.map(movie => Movie.update({ userId: id, title: movie.title }, movie, { upsert: true })
+          .then(result => console.log(result)));
         console.log('found', username, id);
         console.log(`body: 
           events: ${JSON.stringify(events)}, 
